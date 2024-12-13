85d26dfc98e62269c110101e3c3c582b3e007d17
Mangassouba <hamallahmanga@gmail.com>
2024-12-13 13:17:12 +0000

    add controller student

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createStudent, getAllStudents } from '../controllers/studentsController.js';
+import { createStudent, deleteStudent, getAllStudents, getStudentById, updateStudent } from '../controllers/studentsController.js';
 
 const studentRouter = express.Router();
 
 studentRouter.post('/student', createStudent)
 studentRouter.get('/student', getAllStudents)
+studentRouter.get('/student/:id', getStudentById)
+studentRouter.put('/student/:id', updateStudent);
+studentRouter.delete('/student/:id', deleteStudent)
+
 
 export default studentRouter;
